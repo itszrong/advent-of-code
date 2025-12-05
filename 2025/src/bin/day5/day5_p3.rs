057e941d5c7061557de9d09161d15dd0cae4c02e
itszrong <65879824+itszrong@users.noreply.github.com>
2025-12-05 17:17:12 +0000

    2025 day 5 optimal solution

---
+++ 2025/src/bin/day5/day5_p3.rs
@@ -0,0 +1,31 @@
+use std::fs;
+
+fn main() {
+    let contents = fs::read_to_string("src/bin/day5/data.txt")
+        .expect("Should have been able to read the file");
+
+    let mut fresh = 0;
+    let ranges_block = contents.split("\n\n").collect::<Vec<&str>>()[0];
+    let mut ranges: Vec<(i64, i64)> = ranges_block
+        .lines()
+        .filter(|line| !line.trim().is_empty())
+        .map(|line| {
+            let (s, e) = line.split_once('-').unwrap();
+            (s.parse::<i64>().unwrap(), e.parse::<i64>().unwrap())
+        })
+        .collect();
+
+    let mut prev: i64 = -1;
+    ranges.sort_unstable_by_key(|&(s, e)| (s, e));
+    for &(mut s, e) in ranges.iter() {
+        if s <= prev {
+            s = prev + 1;
+        }
+        if s <= e {
+            fresh += e-s+1;
+            prev = e;
+        }
+    }
+    
+    println!("{:?}", fresh);
+}
