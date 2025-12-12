70c837dc8df3ecff1918d5b30ea4bfc7e9dd7c11
itszrong <65879824+itszrong@users.noreply.github.com>
2025-12-12 19:04:32 +0000

    2025 day 11

--- 2025/src/bin/day11/day11_p3.rs
+++ 2025/src/bin/day11/day11_p3.rs
@@ -5,13 +5,14 @@ fn main() {
     let contents = fs::read_to_string("src/bin/day11/data.txt")
         .expect("Should have been able to read the file");
 
-    let mut graph: HashMap<&str, Vec<&str>> = HashMap::new();
-    for line in contents.lines() {
-        let parts = line.split(":").collect::<Vec<&str>>();
-        let key = parts[0];
-        let values = parts[1].split_whitespace().collect::<Vec<&str>>();
-        graph.insert(key, values);
-    }
+    let graph: HashMap<&str, Vec<&str>> = contents
+        .lines()
+        .filter(|line| !line.is_empty())
+        .map(|line| {
+            let (key, values) = line.split_once(":").unwrap();
+            (key, values.split_whitespace().collect::<Vec<&str>>())
+        })
+        .collect();
  
     fn path_find(graph: &HashMap<&str, Vec<&str>>, beginning: &str, end: &str, avoid: &[&str]) -> usize {
 
@@ -21,21 +22,22 @@ fn main() {
         while current_store.len() > 0 {
             let mut new_store: HashMap<&str, usize> = HashMap::new();
             for (&key, &value) in current_store.iter() {
-                let next_nodes = graph.get(&key).unwrap();
-                for next_node in next_nodes {
-                    if avoid.contains(next_node) {
-                        continue
+                if let Some(next_nodes) = graph.get(&key) {
+                    for next_node in next_nodes {
+                        if avoid.contains(next_node) {
+                            continue
+                        }
+                        if next_node == &end {
+                            no_end += value;
+                            continue
+                        }
+                        *new_store.entry(*next_node).or_insert(0) += value;
                     }
-                    if next_node == &end {
-                        no_end += value;
-                        continue
-                    }
-                    *new_store.entry(*next_node).or_insert(0) += value;
                 }
             }
             current_store = new_store;
         }
-        return no_end;
+        no_end
     }
 
     let paths_svr_dac = path_find(&graph, "svr", "dac", &["fft", "out"]);
