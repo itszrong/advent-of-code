3a4efb68e15be91483bad4f400b87825af0e989e
itszrong <65879824+itszrong@users.noreply.github.com>
2025-12-13 17:16:42 +0000

    2024 day 13

---
+++ 2024/day13/day13_p1.py
@@ -0,0 +1,26 @@
+import re
+
+def main():
+    with open("day13/data.txt", "r") as f:
+        contents = f.read()
+
+    parts = contents.split("\n\n")
+
+    tokens = 0
+    for part in parts:
+        x = []
+        y = []
+        lines = part.split('\n')
+        for line in lines:
+            nums = re.findall(r"\d+", line)
+            x.append(int(nums[0]))
+            y.append(int(nums[1]))
+        b = (x[0]*y[2]-x[2]*y[0])/(-x[1]*y[0]+y[1]*x[0])
+        a = (x[2]-x[1]*b)/x[0]
+        if a%1 == 0 and b%1 == 0:
+            tokens += 3*a + b
+    
+    print(int(tokens))
+
+if __name__ == "__main__":
+    main()
