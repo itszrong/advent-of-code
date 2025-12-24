609b5f2abd680cd51d7347539de8508420b4f9cf
itszrong <65879824+itszrong@users.noreply.github.com>
2025-12-25 00:37:33 +0800

    2023 day 1

--- 2023/day1/day1_p1.cpp
+++ 2023/day1/day1_p1.cpp
@@ -1,18 +1,29 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <vector>
 
 int main() {
-    std::ifstream file("day1/data_ex.txt");
+    std::ifstream file("day1/data.txt");
     if (!file.is_open()) {
         std::cerr << "Error opening file" << std::endl;
         return 1;
     }
 
+    
     std::string line;
+    int res = 0;
     while (std::getline(file, line)) {
-        std::cout << line << std::endl;
+        std::vector<char> nums;
+        int l = line.length();
+        for (int i = 0; i <= l; i ++) {
+            if (std::isdigit(line[i])) {
+                nums.push_back(line[i]);
+            }
+        }
+        res += std::stoi(std::string(1, nums[0])+nums.back());
     }
-
+    std::cout << res << std::endl;
+    
     return 0;
 }
--- 2023/day1/day1_p2.cpp
+++ 2023/day1/day1_p2.cpp
@@ -1,18 +1,58 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <vector>
+#include <map>
+using namespace std;
 
 int main() {
-    std::ifstream file("day1/data_ex.txt");
+    ifstream file("day1/data.txt");
     if (!file.is_open()) {
-        std::cerr << "Error opening file" << std::endl;
+        cerr << "Error opening file" << endl;
         return 1;
     }
 
-    std::string line;
-    while (std::getline(file, line)) {
-        std::cout << line << std::endl;
-    }
+    
+    string line;
+    map<string, char> map = {
+        {"one", '1'},
+        {"two", '2'},
+        {"three", '3'},
+        {"four", '4'},
+        {"five", '5'},
+        {"six", '6'},
+        {"seven", '7'},
+        {"eight", '8'},
+        {"nine", '9'}
+    };
 
+    int res = 0;
+    while (getline(file, line)) {
+        vector<char> nums;
+        int l = line.length();
+        string curr;
+        for (int i = 0; i < l; i ++) {
+            curr += line[i];
+
+            if (isdigit(line[i])) {
+                nums.push_back(line[i]);
+            }
+
+            for (const auto &[key, value] : map) {
+                if (curr.ends_with(key)) {
+                    nums.push_back(value);
+                }
+            }
+        }
+
+        if (!nums.empty()) {
+            string val;
+            val.push_back(nums.front());
+            val.push_back(nums.back());
+            res += stoi(val);
+        }
+    }
+    cout << res << endl;
+    
     return 0;
 }
