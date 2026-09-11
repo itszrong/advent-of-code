a3d2f409254e12e25ec3da215b244096a536b814
itszrong <65879824+itszrong@users.noreply.github.com>
2026-09-11 18:45:32 +0100

    2022 day 3

--- 2022/day3/day3_p1.py
+++ 2022/day3/day3_p1.py
@@ -1,8 +1,23 @@
+import string
+
 def main():
-    with open("day3/data_ex.txt", "r") as f:
+    with open("day3/data.txt", "r") as f:
         contents = f.read()
 
-    print(contents)
+    lines = contents.splitlines()
+    result = 0
+    priorities = list(string.ascii_lowercase+string.ascii_uppercase)
+    for line in lines:
+        if len(line)%2 != 0:
+            print('odd')
+        x = line[0:int(len(line)/2)]
+        y = line[int(len(line)/2):]
+        for i in x: 
+            if i in y:
+                result += (priorities.index(i)+1)
+                break
+                
+    print(result)
 
 
 if __name__ == "__main__":
--- 2022/day3/day3_p2.py
+++ 2022/day3/day3_p2.py
@@ -1,8 +1,19 @@
+import string
+
 def main():
-    with open("day3/data_ex.txt", "r") as f:
+    with open("day3/data.txt", "r") as f:
         contents = f.read()
 
-    print(contents)
+    lines = contents.splitlines()
+    result = 0
+    priorities = list(string.ascii_lowercase+string.ascii_uppercase)
+    for i in range(int(len(lines)/3)):
+        for c in lines[3*i]: 
+            if c in lines[3*i+1] and c in lines[3*i+2]:
+                result += (priorities.index(c)+1)
+                break
+                
+    print(result)
 
 
 if __name__ == "__main__":
